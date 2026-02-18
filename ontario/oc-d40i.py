53d147493499af7d84ca261dd98d2a0bc16499b8
Cyril Li <cyrils.li@icloud.com>
2026-02-18 08:52:29 -0600

    update filmed winnipeg, ottawa d60lf

--- ontario/oc-d40i.py
+++ ontario/oc-d40i.py
@@ -16,7 +16,9 @@ years = { # Update from and/or contribute to the CPTDB Wiki
     (4203, 4273, "2004"),
     (4274, 4309, "2005"),
     (4310, 4439, "2005"),
-    (4440, 4526, "2006")
+    (4440, 4526, "2006"),
+    (6351, 6398, "2008"),
+    (6399, 6403, "2009")
 }
 years = {k: v for start, end, v in years for k in range(start, end + 1)}
 
@@ -26,7 +28,8 @@ for entity in feed.entity:
         vehicle_id = int(entity.vehicle.vehicle.id)
     except ValueError:
         continue
-    if vehicle_id >= 4201 and vehicle_id <= 4526: # NFI D40i
+    if ((vehicle_id >= 4201 and vehicle_id <= 4526) or # NFI D40i
+       (vehicle_id >= 6351 and vehicle_id <= 6403)): # NFI D60LF
         statuses.append((entity.vehicle.trip.route_id or "N/A", vehicle_id, entity.vehicle.position.latitude, entity.vehicle.position.longitude, years.get(vehicle_id, "N/A")))
 
 statuses.sort()
